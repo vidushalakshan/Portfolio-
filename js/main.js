916e594b3f3d21d203f78300a959a4234e286147
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-09-23 17:44:59 +0530

    aded js for skills section

--- js/main.js
+++ js/main.js
@@ -33,5 +33,13 @@
         });
     }
 
+    // Skills
+    $('.skill').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+
+
 })(jQuery);
 
